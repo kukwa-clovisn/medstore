328b5c83c57e10c45bf32259586bcde7247f522f
kukwa clovis <kukwaclovisngong3@gmail.com>
2022-07-06 04:45:17 +0100

    commit

--- src/interceptors/axios.js
+++ src/interceptors/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://advancedtechacademy.herokuapp.com/";
-axios.defaults.baseURL = "http://localhost:9005";
+axios.defaults.baseURL = "https://medidrugstore.herokuapp.com/";
+// axios.defaults.baseURL = "http://localhost:9005";
 
 // axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem(
 //   "accessToken"
